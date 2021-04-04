26b73deb7041337c5d2ce94685a9ccecdec378e9
Dhruv Goyal <dhruv.goyal.267@gmail.com>
2021-04-04 15:27:24 +0530

    Added New Solutions

---
+++ (O(n) solution)Merge Two Sorted Array of same size.java
@@ -0,0 +1,23 @@
+int findMedian(int a[], int b[]) {
+        int n = a.length();
+        int i = 0,j = 0;
+        int m1,m2;
+        m1 = m2 = -1;
+        for(int c = 0;c<=n;c++){
+            if(i == n)
+                return b[0];
+            else if(j == n)
+                return a[0];
+            if(a[i]<=b[j]){
+                m1 = m2;
+                m2 = a[i];
+                i++;
+            }
+            else{
+                m1 = m2;
+                m2 = b[j];
+                j++;
+            }
+        }
+        return (m1+m2)/2;
+    }
