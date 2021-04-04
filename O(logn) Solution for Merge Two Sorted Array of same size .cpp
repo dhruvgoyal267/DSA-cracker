0563d779e94acff8795116c73b173e3d248cb90e
Dhruv Goyal <dhruv.goyal.267@gmail.com>
2021-04-04 15:30:16 +0530

    Update O(logn) Solution for Merge Two Sorted Array of same size .cpp

--- O(logn) Solution for Merge Two Sorted Array of same size .cpp
+++ O(logn) Solution for Merge Two Sorted Array of same size .cpp
@@ -1,6 +1,6 @@
-int find_median(int[] a, int[] b, int Sa, int Sb, int Ea, int Eb) {
+int find_median(int a[], int b[], int Sa, int Sb, int Ea, int Eb) {
         if (Ea - Sa <= 1)
-            return (Math.max(a[Sa], b[Sb]) + Math.min(a[Ea], b[Eb])) / 2;
+            return (max(a[Sa], b[Sb]) + min(a[Ea], b[Eb])) / 2;
         int m1 = median(a, Sa, Ea);
         int m2 = median(b, Sb, Eb);
         if (m1 == m2)
