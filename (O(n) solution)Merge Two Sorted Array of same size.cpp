abd968417be24ed7fe78914098ae7bf61359df0e
Dhruv Goyal <dhruv.goyal.267@gmail.com>
2021-04-05 09:18:36 +0530

    Update (O(n) solution)Merge Two Sorted Array of same size.cpp

--- (O(n) solution)Merge Two Sorted Array of same size.cpp
+++ (O(n) solution)Merge Two Sorted Array of same size.cpp
@@ -1,13 +1,19 @@
-int findMedian(int[] a, int[] b) {
+ int findMedian(int[] a, int[] b) {
         int n = a.length;
         int i = 0,j = 0;
         int m1,m2;
         m1 = m2 = -1;
         for(int c = 0;c<=n;c++){
-            if(i == n)
-                return b[0];
-            else if(j == n)
-                return a[0];
+            if(i == n){
+                m1 = m2;
+                m2 = b[0];
+                break;
+            }
+            else if(j == n){
+                m1 = m2;
+                m2 = a[0];
+                break;
+            }
             if(a[i]<=b[j]){
                 m1 = m2;
                 m2 = a[i];
